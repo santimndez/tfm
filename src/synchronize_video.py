17cee4e80042ae51c473366b1e5e3c79ffae6cae
Santiago <gemelosmendez@gmail.com>
2025-12-02 18:38:25 +0100

    3D Animation

--- src/synchronize_video.py
+++ src/synchronize_video.py
@@ -7,7 +7,7 @@ import pandas as pd
 import time
 
 def homog(points):# Convertir a coordenadas homogéneas (vector vertical)
-    return np.vstack((points, np.ones((points.shape[1], 1))))
+    return np.vstack((points, np.ones((1, points.shape[1]))))
 
 def inhomog(homog_points):  # Convertir a coordenadas cartesianas (vector vertical)
     return homog_points[:2, :] / homog_points[2, :][np.newaxis, :]
@@ -56,7 +56,7 @@ def get_fundamental_matrix(M1, M2, C1):
 # Argumentos de la línea de comandos
 parser = argparse.ArgumentParser(description='Sincronizar dos vídeos estéreo minimizando el error de reproyección de la segmentación de la pelota')
 parser.add_argument('-i', '--input', metavar='video_files', type=str, nargs=2, help='Rutas de los dos vídeos a sincronizar')
-parser.add_argument('-o', '--output', metavar='output_file', type=str, help='Ruta del archivo de salida')
+parser.add_argument('-o', '--output', metavar='output_file', type=str, help='Ruta del archivo de salida', default=None)
 parser.add_argument('-m', '--model', metavar='segmentation_model', type=str, help='Ruta del archivo del modelo de segmentación YOLO de la pelota. Si no se proporciona, se deberá proporcionar el parámetro --positions', default=None)
 parser.add_argument('-s', '--separated', action='store_true', help='Si se activa se guardará el primer vídeo desplazado en lugar de un vídeo compuesto over-under')
 parser.add_argument('-k', metavar='camera_matrix', type=str, nargs=2, help='Ruta a los archivos con la matriz de cada cámara en formato .npy', default=None)
@@ -64,22 +64,39 @@ parser.add_argument('-r', '--ref_points', metavar='reference_points', type=str, 
 parser.add_argument('--max_offset', metavar='max_offset', type=int, help='Desfase máximo a considerar en frames (por defecto 600)', default=600)
 parser.add_argument('-p', '--positions', metavar='ball_positions', type=str, nargs=2, help='Ruta a los archivos csv con las posiciones de la pelota en cada frame. Si no se proporciona, se deberá proporcionar el parámetro --model', default=None)
 parser.add_argument('--segment', action='store_true', help='Marca el centro de la pelota detectada en cada frame del vídeo de salida')
+parser.add_argument('--offset', metavar='offset', type=int, help='Desfase en frames a aplicar al primer vídeo (positivo para retrasarlo, negativo para adelantarlo). Si no se proporciona, se estimará automáticamente', default=None)
 args = parser.parse_args()
 
 video_files = args.input
-output_file = args.output
+
+K = [None, None]  # Matrices de cámara
+distortion = [None, None]  # Coeficientes de distorsión
 
 if args.k is None: # Matriz de cámara por defecto
     camera_matrix = np.array([[1.69281160e+03, 0.00000000e+00, 6.81281341e+02],
                               [0.00000000e+00, 1.68036637e+03, 9.03147730e+02],
                               [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
     K = [camera_matrix, camera_matrix]
+    distortion = [np.array([0.13225064, -0.27888641, -0.00721176,  0.03960687, -0.17568582])] * 2
+else:
+    for i in range(2):
+        with open(args.k[i], 'r') as file:
+            lines = file.readlines()
+            K[i] = np.loadtxt(''.join(lines[:3]))
+            distortion[i] = np.loadtxt(lines[3])
 
 # Esquinas de la mesa en el sistema de coordenadas de la cámara en cm
+TABLE_WIDTH = 152.5
+TABLE_LENGTH = 274.0
+TABLE_HEIGHT = 76.0
+NET_HEIGHT = 15.25
+NET_EXTRA_WIDTH = 15.25
+BALL_RADIUS = 2.01
+
 refs = np.array([[0, 0, 0], 
-                 [152.5, 0, 0], 
-                 [152.5, 274, 0], 
-                 [0, 274, 0]], dtype=np.float32) 
+                 [TABLE_WIDTH, 0, 0], 
+                 [TABLE_WIDTH, TABLE_LENGTH, 0], 
+                 [0, TABLE_LENGTH, 0]], dtype=np.float32) # Esquinas en orden antihorario
 points = pd.read_csv(args.ref_points, sep='\t', header=None).values.reshape((2, 4, 2)).astype(np.float32)
 
 M = [np.zeros((3, 4)), np.zeros((3, 4))]  # Inicializar matrices de proyección
@@ -119,6 +136,7 @@ def draw_trajectory(video_file, positions, output_file):
 
 def adjust_video_offset(video_file_1, video_file_2, offset, output_file, separated=False):
     # Crea un vídeo sincronizado usando OpenCV
+    # El offset en frames se aplica al primer vídeo (positivo para retrasarlo, negativo para adelantarlo)
     cap1 = cv.VideoCapture(video_file_1)
     cap2 = cv.VideoCapture(video_file_2)
     fps = cap1.get(cv.CAP_PROP_FPS)
@@ -239,8 +257,8 @@ def get_ball_positions(video_file, model):
             mh, mw = mask.shape
             moments = cv.moments(mask)
             if moments["m00"] != 0:
-                cX = int(w * moments["m10"] / (moments["m00"] * mw))
-                cY = int(h * moments["m01"] / (moments["m00"] * mh))
+                cX = w * moments["m10"] / (moments["m00"] * mw) # No es necesario que sea entero
+                cY = h * moments["m01"] / (moments["m00"] * mh)
                 ball_positions[frame_idx, 0] = cX
                 ball_positions[frame_idx, 1] = cY
                 ball_positions[frame_idx, 2] = 1  # Coordenada homogénea
@@ -301,24 +319,35 @@ if args.model is None and args.positions is None:
 
 model = YOLO(args.model, task='segment', verbose=False) if args.model is not None else None
 
-tic = time.time()
-homog_points1 = get_ball_positions(video_files[0], model).T if args.model is not None else read_ball_positions(args.positions[0]).T
-toc = time.time()
-print(f'Tiempo de obtención de posiciones del vídeo 1: {toc - tic:.2f} segundos')
-print(f'Pelota detectada: {np.sum(homog_points1[2, :])}/{homog_points1.shape[1]} frames')
+if args.model is not None: # Obtener las posiciones de la pelota usando el modelo de segmentación
+    tic = time.time()
+    homog_points1 = get_ball_positions(video_files[0], model).T
+    pd.DataFrame(homog_points1.T).to_csv('positions_video1.csv', index=False)
+    toc = time.time()
+    print(f'Tiempo de obtención de posiciones del vídeo 1: {toc - tic:.2f} segundos')
+    print(f'Pelota detectada: {np.sum(homog_points1[2, :])}/{homog_points1.shape[1]} frames')
 
-tic = time.time()
-homog_points2 = get_ball_positions(video_files[1], model).T if args.model is not None else read_ball_positions(args.positions[1]).T
-toc = time.time()
-print(f'Tiempo de obtención de posiciones del vídeo 2: {toc - tic:.2f} segundos')
-print(f'Pelota detectada: {np.sum(homog_points2[2, :])}/{homog_points2.shape[1]} frames')
+    tic = time.time()
+    homog_points2 = get_ball_positions(video_files[1], model).T
+    pd.DataFrame(homog_points2.T).to_csv('positions_video2.csv', index=False)
+    toc = time.time()
+    print(f'Tiempo de obtención de posiciones del vídeo 2: {toc - tic:.2f} segundos')
+    print(f'Pelota detectada: {np.sum(homog_points2[2, :])}/{homog_points2.shape[1]} frames')
+else:   # Cargar las posiciones de la pelota
+    homog_points1 = read_ball_positions(args.positions[0]).T
+    homog_points2 = read_ball_positions(args.positions[1]).T
 
 # Estimar el desfase entre los dos vídeos
-tic = time.time()
-offset, offset_loss = estimate_offset(F, homog_points1, homog_points2, args.max_offset)
-toc = time.time()
-print(f'Calculando desfases {args.max_offset} - Tiempo: {toc - tic:.2f} segundos')
-print(f'Desfase estimado: {offset} frames, pérdida: {offset_loss[offset + args.max_offset]}')
+if args.offset is not None:
+    offset = args.offset
+    print(f'Usando desfase proporcionado: {offset} frames')
+else:
+    tic = time.time()
+    offset, offset_loss = estimate_offset(F, homog_points1, homog_points2, args.max_offset)
+    offset = -offset
+    toc = time.time()
+    print(f'Calculando desfases {args.max_offset} - Tiempo: {toc - tic:.2f} segundos')
+    print(f'Desfase estimado: {offset} frames, pérdida: {offset_loss[offset + args.max_offset]}')
 
 # Crear el vídeo sincronizado
 if args.segment:
@@ -327,7 +356,92 @@ if args.segment:
     draw_trajectory(video_files[1], homog_points2.T, 'video2_with_trajectory.mp4')
     video_files = ['video1_with_trajectory.mp4', 'video2_with_trajectory.mp4']
 
-tic = time.time()
-adjust_video_offset(video_files[0], video_files[1], -offset, output_file, args.separated)    # Crear el vídeo con los dos vídeos sincronizados
-toc = time.time()
-print(f'El vídeo final ha sido creado en: {output_file} ({toc - tic:.2f} segundos)')
+# Crear el vídeo con los dos vídeos sincronizados
+if args.output is not None:
+    tic = time.time()
+    adjust_video_offset(video_files[0], video_files[1], offset, args.output, args.separated)   
+    toc = time.time()
+    print(f'El vídeo final ha sido creado en: {args.output} ({toc - tic:.2f} segundos)')
+
+# Calcular la posición 3D de la pelota y guardarla en un archivo CSV
+
+nframes = min(homog_points1.shape[1]-max(0, offset), homog_points2.shape[1]-max(0, -offset))
+homog_points1 = homog_points1[:, max(0, offset):max(0, offset) + nframes]
+homog_points2 = homog_points2[:, max(0, -offset):max(0, -offset) + nframes]
+
+# Obtener la trayectoria 3D de la pelota por triangulación
+trajectory = cv.triangulatePoints(M[0], M[1], homog_points1[:2, :], homog_points2[:2, :])
+trajectory /= trajectory[3, :][np.newaxis, :]  # Convertir a coordenadas cartesianas
+trajectory = np.nan_to_num(trajectory, nan=0.0, posinf=0.0, neginf=0.0)
+trajectory[:, homog_points1[2, :] * homog_points2[2, :] == 0] = 0  # Marcar como no válidos los puntos no detectados en alguna cámara
+pd.DataFrame(trajectory.T).to_csv('ball_trajectory_3D.csv', index=False)
+
+# Rellenar los huecos en la trayectoria mediante interpolación lineal
+prev = -1
+last = -1
+for i in range(trajectory.shape[1]):
+    if trajectory[3, i] == 0.0:
+        if prev == -1:
+            prev = i
+    else:
+        if prev != -1:
+            if last != -1:
+                trajectory[:, prev:i] = trajectory[:, last][:, np.newaxis] + ((trajectory[:, i]-trajectory[:, last])[:, np.newaxis] @ np.arange(1, i-last)[np.newaxis, :]/(i-last))
+            else:
+                trajectory[:, prev:i] = trajectory[:, i][:, np.newaxis]
+            prev = -1
+        last = i
+
+# Crea una animación (vídeo) 3D de la trayectoria de la pelota sobre la mesa
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# ax.plot(trajectory[0, :], trajectory[1, :], trajectory[2, :], label='Trayectoria de la pelota')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Trayectoria 3D de la pelota')
+ax.set_xlim3d(-TABLE_WIDTH/2, 1.5*TABLE_WIDTH)
+ax.set_ylim3d(-TABLE_LENGTH/2, 1.5*TABLE_LENGTH)
+ax.set_zlim3d(-TABLE_HEIGHT, 3*TABLE_HEIGHT)
+
+
+# Dibujar superficie de la mesa
+xx, yy = np.meshgrid([0, TABLE_WIDTH], [0, TABLE_LENGTH])
+zz = np.full_like(xx, 0)
+ax.plot_surface(xx, yy, zz, color='blue', alpha=0.5)
+
+# Dibujar líneas de la mesa
+ax.plot([0, TABLE_WIDTH], [0, 0], [0, 0], color='white')
+ax.plot([TABLE_WIDTH, TABLE_WIDTH], [0, TABLE_LENGTH], [0, 0], color='white')
+ax.plot([TABLE_WIDTH, 0], [TABLE_LENGTH, TABLE_LENGTH], [0, 0], color='white')
+ax.plot([0, 0], [TABLE_LENGTH, 0], [0, 0], color='white')
+ax.plot([TABLE_WIDTH/2, TABLE_WIDTH/2], [0, TABLE_LENGTH], [0, 0], color='white')
+
+# Dibujar la red
+x_net = np.linspace(-NET_EXTRA_WIDTH, TABLE_WIDTH + NET_EXTRA_WIDTH, 40)
+z_net = np.linspace(0,  NET_HEIGHT, 10)
+XX, ZZ = np.meshgrid(x_net, z_net)
+YY = np.full_like(XX, TABLE_LENGTH/2)
+
+# Dibujar la red como una malla
+ax.plot_surface(XX, YY, ZZ, color='black', alpha=0.3)
+
+# Punto de la pelota
+ball, = ax.plot([], [], [], 'o', color='orange', markersize=5)
+
+def update(frame, ball, trajectory):
+    x = trajectory[0, frame]
+    y = trajectory[1, frame]
+    z = trajectory[2, frame]
+    ball.set_data([x], [y])
+    ball.set_3d_properties([z])
+    return ball,
+
+ani = FuncAnimation(fig, update, frames=trajectory.shape[1], fargs = (ball, trajectory), interval=32, blit=False, repeat=True)
+
+# ani.save('ball_trajectory_3D.mp4', writer='ffmpeg', fps=60)
+plt.show()
